b6cb5aaa27554e2dcbc59d1f9155897b3030cca1
xuhuaiyu <wowxhycoming@sina.com>
2016-11-18 16:01:38 +0800

    sub router 的排序方式

--- java.vertx.web/src/main/java/me/xhy/java/vertx/web/o3router/O5SubRouterOrder.java
+++ java.vertx.web/src/main/java/me/xhy/java/vertx/web/o3router/O5SubRouterOrder.java
@@ -19,28 +19,29 @@ public class O5SubRouterOrder extends AbstractVerticle {
 		System.out.println("[Deployed]" + Thread.currentThread().getName());
 
 		Router router = Router.router(vertx);
-		router.route("/path/*").order(3).handler(new Router1Handler());
-		router.route("/path/sub").order(1).handler(new Router2Handler());
+		router.route("/path/*").order(3).handler(new Router1Handler()); // mountSubRouter index +0
+		router.route("/path/sub").order(1).handler(new Router2Handler()); // mountSubRouter index +1
 
 		Router subRouter1 =  Router.router(vertx);
 		subRouter1.route("/sub").order(1).handler(new SubRouter1Handler("A"));
 		subRouter1.route("/sub").order(2).handler(new SubRouter2Handler("A"));
 		subRouter1.route("/sub").order(3).handler(new SubRouter3Handler("A"));
-		router.mountSubRouter("/path", subRouter1);
+		router.mountSubRouter("/path", subRouter1); // mountSubRouter index +2 ， order =2
 
-		router.route("/path/*").order(0).handler(new Router3Handler());
+		router.route("/path/*").order(0).handler(new Router3Handler()); // mountSubRouter index +3
 
 		Router subRouter2 = Router.router(vertx);
 		subRouter2.route("/sub").order(3).handler(new SubRouter1Handler("B"));
 		subRouter2.route("/sub").order(2).handler(new SubRouter2Handler("B"));
 		subRouter2.route("/sub").order(1).handler(new SubRouter3Handler("B"));
-		router.mountSubRouter("/path", subRouter2);
+		router.mountSubRouter("/path", subRouter2); // mountSubRouter index +4 ， order = 4
 
 		router.route("/path/sub").order(-1).handler(new Router4Handler());
 
 		router.route("/path/*").order(-2).handler(new Router5Handler());
 
 		router.route("/path/*").last().handler(new EndHandler());
+		// order 相等的， 按代码先后顺序
 
 		server.requestHandler(router::accept)
 				.listen(config().getInteger("http.port",10016), result -> {
