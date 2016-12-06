2ff8a5d48c3989da29c233f1b3dc55b9dfc15826
wowxhycoming <wowxhycoming@sina.com>
2016-12-06 15:07:44 +0800

    修改配置让输出更有意义

--- java.vertx.microservice/quote/src/main/java/me/xhy/java/vertx/microservice/quote/MarketDataVerticle.java
+++ java.vertx.microservice/quote/src/main/java/me/xhy/java/vertx/microservice/quote/MarketDataVerticle.java
@@ -12,118 +12,121 @@ import java.util.Random;
  */
 public class MarketDataVerticle extends AbstractVerticle {
 
-  String name;
-  int variation;
-  long period;
-  String symbol;
-  int stocks;
-  double price;
+	String name;
+	int variation;
+	long period;
+	String symbol;
+	int stocks;
+	double price;
 
-  double bid;
-  double ask;
+	double bid;
+	double ask;
 
-  int share;
-  private double value;
+	int share;
+	private double value;
 
-  private final Random random = new Random();
+	private final Random random = new Random();
 
-  /**
-   * Method called when the verticle is deployed.
-   */
-  @Override
-  public void start() {
-    System.out.println(MarketDataVerticle.class.getName() + "===" + period + "===");
-    // Retrieve the configuration, and initialize the verticle.
-    JsonObject config = config();
-    init(config);
+	/**
+	 * Method called when the verticle is deployed.
+	 */
+	@Override
+	public void start() {
+		System.out.println(MarketDataVerticle.class.getName() + "===" + period + "===");
+		// Retrieve the configuration, and initialize the verticle.
+		JsonObject config = config();
+		System.out.println("config ========== \r\n" + config);
+		init(config);
 
-    // Every `period` ms, the given Handler is called.
-    vertx.setPeriodic(period, l -> {
-      compute();
-      send();
-    });
-  }
+		// Every `period` ms, the given Handler is called.
+		System.out.println("period ================== " + period);
+		vertx.setPeriodic(period, l -> {
+			compute();
+			send();
+		});
+	}
 
-  /**
-   * Read the configuration and set the initial values.
-   * @param config the configuration
-   */
-  void init(JsonObject config) {
-    period = config.getLong("period", 3000L);
-    variation = config.getInteger("variation", 100);
-    name = config.getString("name");
-    Objects.requireNonNull(name);
-    symbol = config.getString("symbol", name);
-    stocks = config.getInteger("volume", 10000);
-    price = config.getDouble("price", 100.0);
+	/**
+	 * Read the configuration and set the initial values.
+	 *
+	 * @param config the configuration
+	 */
+	void init(JsonObject config) {
+		period = config.getLong("period", 3000L);
+		variation = config.getInteger("variation", 100);
+		name = config.getString("name");
+		Objects.requireNonNull(name);
+		symbol = config.getString("symbol", name);
+		stocks = config.getInteger("volume", 10000);
+		price = config.getDouble("price", 100.0);
 
-    value = price;
-    ask = price + random.nextInt(variation / 2);
-    bid = price + random.nextInt(variation / 2);
+		value = price;
+		ask = price + random.nextInt(variation / 2);
+		bid = price + random.nextInt(variation / 2);
 
-    share = stocks / 2;
-  }
+		share = stocks / 2;
+	}
 
-  /**
-   * Sends the market data on the event bus.
-   */
-  private void send() {
-    JsonObject jo = toJson();
-    System.out.println(jo);
-    vertx.eventBus().publish(GeneratorConfigVerticle.ADDRESS, jo);
-    System.out.println("publish|||" + Thread.currentThread().getName());
-  }
+	/**
+	 * Sends the market data on the event bus.
+	 */
+	private void send() {
+		JsonObject jo = toJson();
+		System.out.println(jo);
+		vertx.eventBus().publish(GeneratorConfigVerticle.ADDRESS, jo);
+		System.out.println("publish|||" + Thread.currentThread().getName());
+	}
 
-  /**
-   * Compute the new evaluation...
-   */
-  void compute() {
+	/**
+	 * Compute the new evaluation...
+	 */
+	void compute() {
 
-    if (random.nextBoolean()) {
-      value = value + random.nextInt(variation);
-      ask = value + random.nextInt(variation / 2);
-      bid = value + random.nextInt(variation / 2);
-    } else {
-      value = value - random.nextInt(variation);
-      ask = value - random.nextInt(variation / 2);
-      bid = value - random.nextInt(variation / 2);
-    }
+		if (random.nextBoolean()) {
+			value = value + random.nextInt(variation);
+			ask = value + random.nextInt(variation / 2);
+			bid = value + random.nextInt(variation / 2);
+		} else {
+			value = value - random.nextInt(variation);
+			ask = value - random.nextInt(variation / 2);
+			bid = value - random.nextInt(variation / 2);
+		}
 
-    if (value <= 0) {
-      value = 1.0;
-    }
-    if (ask <= 0) {
-      ask = 1.0;
-    }
-    if (bid <= 0) {
-      bid = 1.0;
-    }
+		if (value <= 0) {
+			value = 1.0;
+		}
+		if (ask <= 0) {
+			ask = 1.0;
+		}
+		if (bid <= 0) {
+			bid = 1.0;
+		}
 
-    if (random.nextBoolean()) {
-      // Adjust share
-      int shareVariation = random.nextInt(100);
-      if (shareVariation > 0 && share + shareVariation < stocks) {
-        share += shareVariation;
-      } else if (shareVariation < 0 && share + shareVariation > 0) {
-        share += shareVariation;
-      }
-    }
-  }
+		if (random.nextBoolean()) {
+			// Adjust share
+			int shareVariation = random.nextInt(100);
+			if (shareVariation > 0 && share + shareVariation < stocks) {
+				share += shareVariation;
+			} else if (shareVariation < 0 && share + shareVariation > 0) {
+				share += shareVariation;
+			}
+		}
+	}
 
-  /**
-   * @return a json representation of the market data (quote). The structure is close to
-   * <a href="https://en.wikipedia.org/wiki/Market_data">https://en.wikipedia.org/wiki/Market_data</a>.
-   */
-  private JsonObject toJson() {
-    return new JsonObject()
-        .put("exchange", "vert.x stock exchange")
-        .put("symbol", symbol)
-        .put("name", name)
-        .put("bid", bid)
-        .put("ask", ask)
-        .put("volume", stocks)
-        .put("open", price)
-        .put("shares", share);
+	/**
+	 * @return a json representation of the market data (quote). The structure is close to
+	 * <a href="https://en.wikipedia.org/wiki/Market_data">https://en.wikipedia.org/wiki/Market_data</a>.
+	 */
+	private JsonObject toJson() {
+		return new JsonObject()
+				.put("exchange", "vert.x stock exchange")
+				.put("symbol", symbol)
+				.put("name", name)
+				.put("bid", bid)
+				.put("ask", ask)
+				.put("volume", stocks)
+				.put("open", price)
+				.put("shares", share);
 
-  }
+	}
 }
