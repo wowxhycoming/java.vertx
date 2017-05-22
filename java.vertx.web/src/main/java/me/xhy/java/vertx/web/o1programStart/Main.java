d296aa37d742fcc0d5ad2292ade792956da9f831
xuhuaiyu <wowxhycoming@sina.com>
2017-05-22 20:44:41 +0800

    备注java.vertx.web的o1包

--- java.vertx.web/src/main/java/me/xhy/java/vertx/web/o1programStart/Main.java
+++ java.vertx.web/src/main/java/me/xhy/java/vertx/web/o1programStart/Main.java
@@ -20,6 +20,10 @@ public class Main {
 
         final VertxOptions vertxOptions = Options.getVertxOptions("VXWEB.");
         final VertxFactory factory = new VertxFactoryImpl();
+        /**
+         * 因为启用了 cluster 集群模式，所以不能直接使用 factory.vertx() 方法来创建 Vert.x 实例
+         * 所以关闭了 cluster 集群模式，opts.setClustered(false)
+         */
         final Vertx vertx = factory.vertx(vertxOptions);
         final DeploymentOptions verticleOpts = Options.standardDeploymentOptions();
         vertx.deployVerticle(RouterVerticle.class.getName(), verticleOpts);
--- java.vertx.web/src/main/java/me/xhy/java/vertx/web/o1programStart/RouterVerticle.java
+++ java.vertx.web/src/main/java/me/xhy/java/vertx/web/o1programStart/RouterVerticle.java
@@ -10,16 +10,26 @@ public class RouterVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        // createHttpServer还有一个重载方法，可传入一个io.vertx.core.http.HttpServerOptions实例，
-        // 该实例可以进行Web Server级别的配置，也可以理解成为包含Vert.x实例的容器配置项信息，这里就不做说明了。
+        /**
+         * createHttpServer 还有一个重载方法，可传入一个 io.vertx.core.http.HttpServerOptions 实例，
+         * 该实例可以进行 Web Server 级别的配置，也可以理解成为包含 Vert.x 实例的容器配置项信息
+         */
         final HttpServer server = vertx.createHttpServer();
 
-        // 同一个类型的Verticle实例可共享端口，如同上边的代码演示的，10个Verticle实例共享了8080的端口。
-        // 不同的Verticle实例目前没找到共享端口的方法，暂时定义为不能共享端口。
-        // 如果两个不同的Verticle监听了同一个端口会出现JVM_BIND的端口占用异常。
+        /**
+         * 同一个类型的 Verticle 实例可共享端口，如同上边的代码演示的，10 个 Verticle 实例共享了 8080 的端口。
+         * 不同的Verticle实例目前没找到共享端口的方法，暂时定义为不能共享端口。
+         * 果两个不同的Verticle监听了同一个端口会出现JVM_BIND的端口占用异常。
+         *
+         * 控制台会打印: "部署：vert.x-eventloop-thread-0" ~ "部署：vert.x-eventloop-thread-9" (配置文件中的 instances 为 10)，
+         * 从打印顺序不固定可以看出， deploy 过程是异步的。
+         */
         System.out.println("部署：" + Thread.currentThread().getName());
 
         server.requestHandler(request -> {
+            /**
+             * 访问成功后，按住 F5 刷新浏览器，会看到不同的线程再处理请求
+             */
             System.out.println("Handler:" + Thread.currentThread().getName());
             request.response().end("The First Vert.x Web Demo");
         });
