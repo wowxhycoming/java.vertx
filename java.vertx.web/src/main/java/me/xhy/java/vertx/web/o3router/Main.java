ba6fa7d33470095be9f92cbf0e37ab64e043cfb8
xuhuaiyu <wowxhycoming@sina.com>
2017-05-22 22:17:15 +0800

    增加 o3 包中 StandardVerticle 启动方法

---
+++ java.vertx.web/src/main/java/me/xhy/java/vertx/web/o3router/Main.java
@@ -0,0 +1,33 @@
+package me.xhy.java.vertx.web.o3router;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.impl.VertxFactoryImpl;
+import io.vertx.core.spi.VertxFactory;
+import me.xhy.java.vertx.web.util.Options;
+
+/**
+ * Created by xuhuaiyu on 2016/11/13.
+ */
+public class Main {
+
+    public static void main(final String... args) {
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+        final VertxOptions vertxOptions = Options.getVertxOptions("VXWEB.");
+        final VertxFactory factory = new VertxFactoryImpl();
+        final Vertx vertx = factory.vertx(vertxOptions);
+        final DeploymentOptions verticleOpts = Options.standardDeploymentOptions();
+
+        vertx.deployVerticle(O1Router.class.getName(), verticleOpts);
+        vertx.deployVerticle(O2RouterOrder.class.getName(), verticleOpts);
+        vertx.deployVerticle(O3BlockingHandler.class.getName(), verticleOpts);
+        vertx.deployVerticle(O4SubRouter.class.getName(), verticleOpts);
+        vertx.deployVerticle(O5SubRouterOrder.class.getName(), verticleOpts);
+        vertx.deployVerticle(O6FailureHandler.class.getName(), verticleOpts);
+
+    }
+
+}
